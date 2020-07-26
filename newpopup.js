576687aa3abb97884a58d481ff522ea06b61eec9
Dom W <domiwar.it@gmail.com>
2020-07-26 13:34:11 -0400

    hyperlinks and popups added

---
+++ newpopup.js
@@ -0,0 +1,11 @@
+
+// When the user clicks on <div>, open the popup
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
+function myFunction() {
+    var pop2up = document.getElementById("contactMe");
+    pop2up.classList.toggle("show");
+  }
